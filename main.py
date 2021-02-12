b0088890a6f539bc68aefb7e1ef7025cbf4473fe
Slightowl <slightowler7@gmail.com>
2021-02-12 10:46:39 +0000

    updates

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from instascrape import *
-import re
 import pandas as pd
 import socks
 import socket
@@ -45,6 +44,7 @@ if __name__ == "__main__":
     )
     socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=SOCKS_PORT)
     socket.socket = socks.socksocket
+    print(socket.socket)
     print("tor takeoff 🚀➡️🌑")
     get_handles()
     tor_process.kill()
